7e6b690ed8efce9c6dcf6fc3fb12d2e329bf5e65
Max R <maxime-ruest@hotmail.com>
2022-12-15 18:52:03 -0500

    Update testing.js

--- src/_tests_/testing.js
+++ src/_tests_/testing.js
@@ -22,5 +22,11 @@ const ValidateEmail = (input) => {
   }
   
   
+  console.log("testing email validation (bob) should return false")
   console.log(ValidateEmail("bob"))
+  console.log("testing email validation (bob@hotmail.com) should return true")
+  console.log(ValidateEmail("bob@hotmail.com"))
+  console.log("testing password validation (123) should return false")
+  console.log(validatePassword("123"))
+  console.log("testing password validation (12345) should return true")
   console.log(validatePassword("12345"))
